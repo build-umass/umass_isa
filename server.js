334ae13b7878ed5fe54bdf23f559b8229a4ee930
Simona Zilberberg <simonazilber@umass.edu>
2023-01-21 21:40:28 -0500

    Added CORS Error functionality

--- server.js
+++ server.js
@@ -2,9 +2,19 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const { logger } = require('./middleware/logger')
+const errorHandler = require('./middleware/errorHandler')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
 const port = process.env.PORT || 3500;
 
 app.use(logger)
+
+app.use(cors(corsOptions))
+
+app.use(express.json())
+
+app.use(cookieParser( ))
 
 app.use('/', express.static(path.join(__dirname, 'public')))
 
@@ -20,5 +30,7 @@ app.all('*', (req,res) => {
         res.type('txt').send('404 Not Found')
     }
 })
+
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
