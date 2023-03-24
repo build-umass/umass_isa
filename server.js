ab29f84812c73c35e0d85be58b1e8fada5da770c
Simona Zilberberg <simonazilber@umass.edu>
2023-03-24 15:11:53 -0400

    added more middleware comments

--- server.js
+++ server.js
@@ -7,15 +7,18 @@ const cookieParser = require('cookie-parser')
 const cors = require('cors')
 const corsOptions = require('./config/corsOptions')
 const port = process.env.PORT || 3500;
-
+ 
 app.use(logger)
 
 app.use(cors(corsOptions))
 
+// allows app to recieve and parse json data
 app.use(express.json())
 
 app.use(cookieParser( ))
 
+//express.static: middleware that is telling our server where to grab static files
+//'public' represents a folder in our codebase 
 app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.use('/', require('./routes/root'))
