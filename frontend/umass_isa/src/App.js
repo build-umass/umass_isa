92f195ade4819beb529ef385f1c5b582b7144ed2
Simona Zilberberg <simonazilber@umass.edu>
2023-04-03 17:00:50 -0400

    added public , which is the facing page component

--- frontend/umass_isa/src/App.js
+++ frontend/umass_isa/src/App.js
@@ -1,11 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import Layouts from './components/Layouts'
+import Public from './components/Public'
 
 function App() {
   return (
     <Routes>
+      {/* this element is the parent route of everything else*/}
       <Route path="/" element={<Layouts />}>
-
+        <Route index element={<Public />} />
+        {/* public links to a login component, 11:18 video 5 */}
       </Route>
     </Routes>
   );
---
+++ frontend/umass_isa/src/components/Public.js
@@ -0,0 +1,16 @@
+// public facing page
+// need link from react router 
+import { Link } from 'react-router-dom'
+
+const Public = () => {
+    //basic web page
+    const content = (
+        <header>
+            <h1> Welcome! </h1>
+        </header>
+    )
+
+    return content
+}
+
+export default Public
